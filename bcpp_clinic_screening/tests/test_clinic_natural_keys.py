00d4328a4e98b6a69b6e2a2c5976b9a788837bb9
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-06-29 12:06:47 +0200

    fix import errors

--- bcpp_clinic_screening/tests/test_clinic_natural_keys.py
+++ bcpp_clinic_screening/tests/test_clinic_natural_keys.py
@@ -1,13 +1,16 @@
 from django.test import TestCase, tag
 
-from edc_sync.test_mixins import SyncTestSerializerMixin
+from edc_sync.tests.sync_test_helper import SyncTestHelper
 
 
 @tag('natural_key')
-class TestNaturalKey(SyncTestSerializerMixin, TestCase):
+class TestNaturalKey(TestCase):
+
+    sync_helper = SyncTestHelper()
 
     def test_natural_key_attrs(self):
-        self.sync_test_natural_key_attr('bcpp_clinic_screening')
+        self.sync_helper.sync_test_natural_key_attr('bcpp_clinic_screening')
 
     def test_get_by_natural_key_attr(self):
-        self.sync_test_get_by_natural_key_attr('bcpp_clinic_screening')
+        self.sync_helper.sync_test_get_by_natural_key_attr(
+            'bcpp_clinic_screening')
