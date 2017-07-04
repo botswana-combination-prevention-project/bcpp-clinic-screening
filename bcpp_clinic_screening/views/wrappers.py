1df9568b98bc21079ca3aedb286aa0e5e44dda2b
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-07-04 12:59:50 +0200

    move an import inside the method

--- bcpp_clinic_screening/views/wrappers.py
+++ bcpp_clinic_screening/views/wrappers.py
@@ -1,43 +1,41 @@
 from django.apps import apps as django_apps
-# from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist
 
-# from edc_base.utils import get_uuid
-# from edc_consent.site_consents import site_consents
+from edc_base.utils import get_uuid
+from edc_consent.site_consents import site_consents
 from edc_model_wrapper import ModelWrapper
-
-# from bcpp_clinic_subject.views import SubjectConsentModelWrapper
 
 
 class ConsentMixin:
 
-    pass
-#     @property
-#     def consent_object(self):
-#         """Returns the consent model.
-#         """
-#         default_consent_group = django_apps.get_app_config(
-#             'edc_consent').default_consent_group
-#         consent_object = site_consents.get_consent(
-#             report_datetime=self.object.report_datetime,
-#             consent_group=default_consent_group)
-#         return consent_object
+    @property
+    def consent_object(self):
+        """Returns the consent model.
+        """
+        default_consent_group = django_apps.get_app_config(
+            'edc_consent').default_consent_group
+        consent_object = site_consents.get_consent(
+            report_datetime=self.object.report_datetime,
+            consent_group=default_consent_group)
+        return consent_object
 
-#     @property
-#     def consent(self):
-#         """Returns a wrapped saved or unsaved consent.
-#         """
-#         consent_model_wrapper_class = SubjectConsentModelWrapper
-#         try:
-#             consent = self.consent_object.model.objects.get(
-#                 version=self.consent_object.version,
-#                 screening_identifier=self.object.screening_identifier)
-#         except ObjectDoesNotExist:
-#             consent = self.consent_object.model(
-#                 subject_identifier=self.object.subject_identifier,
-#                 consent_identifier=get_uuid(),
-#                 screening_identifier=self.object.screening_identifier,
-#                 version=self.consent_object.version)
-#         return consent_model_wrapper_class(consent)
+    @property
+    def consent(self):
+        """Returns a wrapped saved or unsaved consent.
+        """
+        from bcpp_clinic_subject.views import SubjectConsentModelWrapper
+        consent_model_wrapper_class = SubjectConsentModelWrapper
+        try:
+            consent = self.consent_object.model.objects.get(
+                version=self.consent_object.version,
+                screening_identifier=self.object.screening_identifier)
+        except ObjectDoesNotExist:
+            consent = self.consent_object.model(
+                subject_identifier=self.object.subject_identifier,
+                consent_identifier=get_uuid(),
+                screening_identifier=self.object.screening_identifier,
+                version=self.consent_object.version)
+        return consent_model_wrapper_class(consent)
 
 
 class SubjectEligibilityModelWrapper(ConsentMixin, ModelWrapper):
